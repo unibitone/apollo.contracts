89fe16f8b3f9b88aa91546596e700f49e999dae2
edison <1433505987@qq.com>
2022-05-19 11:55:47 +0800

    apollo数据表提交

--- contracts/apollo.sttle/include/apollo.sttle/apollo.sttle.db.hpp
+++ contracts/apollo.sttle/include/apollo.sttle/apollo.sttle.db.hpp
@@ -15,6 +15,8 @@
 
 using namespace eosio;
 
+#define HASH256(str) sha256(const_cast<char*>(str.c_str()), str.size());
+
 #define SYMBOL(sym_code, precision) symbol(symbol_code(sym_code), precision)
 
 namespace apollo {
@@ -24,109 +26,44 @@ using namespace eosio;
 using namespace wasm;
 
 static constexpr eosio::name active_perm{"active"_n};
+static constexpr uint64_t start_time_since_epoch   = 1655569098;
+static constexpr uint64_t seconds_per_day       = 24 * 3600;
 
 #define STTLE_TBL [[eosio::table, eosio::contract("apollo.sttle")]]
 
-struct hashrate_t {
-    float value;
-    char unit;
-};
-
-struct time {
-    int hour;
-    int minute;
-
-    time() {}
-    
-    time(int h, int m){
-        this->hour = h;
-        this->minute = m;
-    }
-
-    bool operator == ( const time& time1, const int& time2 ){
-        if (time1.hour == time2.hour && time1.minute == time2.minute)
-            return true;
-        return false;
-    }
-    
-    bool operator <= ( const time& time1, const int& time2 ){
-        if ( time1.hour < time2.hour ){
-
-            return true;
-        }else if (time1.hour > time2.hour){
-
-            return false;
-        }
-            
-        if ( time1.minute < time2.minute ){
-
-            return true;
-        }else if ( time1.minute > time2.minute ){
-
-           return false; 
-        }
-
-        return true;
-    }
-    
-    bool operator >= ( const time& time1, const int& time2 ){
-
-        if ( time1.hour > time2.hour ){
-
-            return true;
-        }else if (time1.hour < time2.hour){
-
-            return false;
-        }
-            
-        if ( time1.minute > time2.minute ){
-
-            return true;
-        }else if ( time1.minute < time2.minute ){
-
-           return false; 
-        }
-
-        return true;
-    }
-
-};
-
-struct [[eosio::table("global"), eosio::contract("apollo.sttle")]] global_t {
-    hashrate_t          hashrate;
-    asset               asset;
-    time                check_begin;
-    time                check_end;
-    time                sttle_begin;
-    time                sttle_end;
-
-
-    global_t() {}
-
-    EOSLIB_SERIALIZE( global_t, (hashrate)(asset)(check_begin)(check_end)(sttle_begin)(sttle_end) )
-};
-
-typedef eosio::singleton< "global"_n, global_t > global_singleton;
 
 struct STTLE_TBL sttle_t {
     name        owner;
-    string      nft_id;
+    name        beneficiary;
+    uint32_t    token_id;
+    uint32_t    sub_token_id;
     asset       profit;
-    time_point  mine_at;
-    time_point  start_at;
+    time_point  last_mine_at;
+    uint16_t    sttle_times = 0;
+    uint16_t    status = 0;
+    uint64_t    primary_key()const { 
+        string str =    owner.to_string() + "\n" + 
+                        to_string(token_id) + "\n" +
+                        to_string(sub_token_id);
 
-    uint64_t    primary_key()const { return owner.value; }
+        return static_cast<uint64_t> ( HASH256(str) );
+
+    }
+
     uint64_t    scope() const { return 0; }
-    uint64_t    by_mine_coin()const { return name(asset_meta.mine_coin_type).value; }
+    // uint64_t    by_token_id()const { return (uint64_t) token_id << 32; }
+    // uint64_t    by_sub_token_id()const { return (uint64_t) sub_token_id << 32; }
 
     sttle_t() {}
-    sttle_t(const name& c): claimer(c) {}
+    sttle_t(const name& owner,const uint32_t& t_id,const uint32_t& s_t_id): owner(c), token_id(t_id), sub_token_id(s_t_id) {}
 
-    EOSLIB_SERIALIZE( sttle_t, (owner)(nft_id)(profit)(mine_at)(start_at) )
-
-    typedef eosio::multi_index<"sttles"_n, sttle_t,
-            indexed_by<"nftid"_n, const_mem_fun<sttle_t, uint64_t, &sttle_t::by_mine_coin> >
+    typedef eosio::multi_index
+            <"sttles"_n, sttle_t,
+            // indexed_by<"token_id"_n, const_mem_fun<sttle_t, uint64_t, &sttle_t::by_token_id> >,
+            // indexed_by<"sub_token_id"_n, const_mem_fun<sttle_t, uint64_t, &sttle_t::by_sub_token_id> >
      > tbl_t;
+
+     EOSLIB_SERIALIZE( sttle_t, (owner)(beneficiary)(sub_token_id)(profit)(last_mine_at)(sttle_times)(status) )
 };
 
 } // apollo
